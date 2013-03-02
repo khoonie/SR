4b41e08d022cdc68587821620aa691d25d7b84f4
richard <khoonie@gmail.com>
2013-03-02 15:33:02 +0800

    Initial Commit

---
+++ stockrobot/MasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  MasterViewController.h
+//  stockrobot
+//
+//  Created by BMM on 13/3/2.
+//  Copyright (c) 2013年 BMM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@end
---
+++ stockrobot/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  stockrobot
+//
+//  Created by BMM on 13/3/2.
+//  Copyright (c) 2013年 BMM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
